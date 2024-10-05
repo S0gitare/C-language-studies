3afe9824b350738ae7647f7b0b2f00b07fd2c449
Alecsander Lima <alecsanderdavinci@gmail.com>
2024-10-05 00:13:23 -0300

    amanha continuo

--- seção 6/Exercícios de fixação/Exercicio for 01.c
+++ seção 6/Exercícios de fixação/Exercicio for 01.c
@@ -2,17 +2,21 @@
 //* Adicionando o valor atual de I numa variável SOMA, a cada repetição do laço FOR
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main() {
     int i;
     int soma = 0;
 
     for (i = 0; i < 10; i++) {
-        printf("%d \n ", i);
+        printf("\n i = %d (soma: %d) \n ", i, soma);
+    
+        soma = (soma + i);
+        sleep(1);
     }
+    printf(" \n Soma %d \n", soma);
 
-    soma = (soma + i);
-    printf(" \n A soma de todas as repeticoes e igual a: %d ", soma);
-
+    system("pause");
     return 0;
 }
---
+++ seção 6/Exercícios de fixação/Exercicio for 02.c
@@ -0,0 +1,22 @@
+//* Faça um programa que que mostra uma tabuada
+//* De um número informado pelo usuário
+
+#include <stdio.h>
+
+int main() {
+    int N1, i;
+
+    printf("Digite um número: ");
+    scanf("%d", &N1);
+
+    printf("\n ---------------------\n");
+    printf("\n    Tabuada do %d:\n", N1);
+    printf("\n ----------------------\n");
+
+    for (i = 0; i < N1; i++) {
+        
+    }
+
+    system("pause");
+    return 0;
+}
