4f52893dd7f9237ec4bb2d3a12ab48dd2644c885
Alecsander Lima <alecsanderdavinci@gmail.com>
2024-10-10 16:57:01 -0300

    depois continuo

--- Seção 11/alocacao dinamica.cpp
+++ Seção 11/alocacao dinamica.cpp
@@ -1,1 +1,33 @@
-//* fica par amamnha
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <stdbool.h>
+using namespace std;
+
+int* alocavetor(int tamanho);
+
+int main(){
+    int *vetor; int tamanho, cont;
+    cout << "Digite o tamanho do vetor: ";
+    cin >> tamanho;
+    vetor = alocavetor(tamanho);
+    for (cont=0; cont<tamanho; cont++){
+        cout << vetor[cont] << "\n";
+    }
+    system("pause");
+    return 0;
+}
+int* alocavetor(int tamanho){
+    int *aux;
+    //* Alocação dinamica de memoria
+    aux = (int*) malloc(tamanho * sizeof(int));
+    return aux;
+}
+
+
+
+
+
+
+
