5869771faa1c9c44714afcee928eeaefda9e2c52
Alecsander Lima <alecsanderdavinci@gmail.com>
2024-10-19 18:33:56 -0300

    melhor deixar pra aprender mais quando eu terminar orientação a objetos

--- .UsandoSFML/codigo_anterior.cpp
+++ .UsandoSFML/codigo_anterior.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
+#include <new>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include <SFML/Graphics.hpp>
-
+using namespace std;
 using sf::RenderWindow;
 
 int main() {
     RenderWindow janela(sf::VideoMode(800, 600), "minha primeira janela");
+
+    sf::Text teste;
+    teste.setCharacterSize(24);
+    teste.setFillColor(sf::Color::White);
+    teste.setString("Olá mundo!");
 
     while (janela.isOpen()) {
         sf::Event event;
@@ -15,6 +26,7 @@ int main() {
         janela.clear(sf::Color::Black);
         janela.display();
     }
+    
 
     return 0;
 }
