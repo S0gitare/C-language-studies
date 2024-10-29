a572274c4c6237f5d2c8a8dbb3a59e222e3a6a70
Alecsander Lima <alecsanderdavinci@gmail.com>
2024-10-29 16:58:06 -0300

    bora la

---
+++ Seção 27 - Herança, Polimorfismo e abstração/Exercicios/Exercicio 01.cpp
@@ -0,0 +1,10 @@
+//* Faça um programa com uma CLASSE ANIMAL com TERRENO, seus getters e setters.
+//* Depois uma CLASSE MAMIFERO FILHA DE ANIMAL, com mesesgestação, seus getters e setters.
+//* Depois uma CLASSE CACHORRO FILHA DE MAMIFERO, com raça, seus getters e setters e um método para latir.
+
+//* Depois crie UM CACHORRO COM TERRENO, MESESGESTAÇÃO, RAÇA E FAÇA-O LATIR.
+//! OBS: Faça primeiro um diagrama UML, depois codifique o programa
+
+#include <iostream>
+using namespace std;
+
